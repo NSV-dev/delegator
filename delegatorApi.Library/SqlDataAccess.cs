f0c6f6028dbfadbd163032240524d5a509084492
nsv-dev <nsvkart@bk.ru>
2022-02-02 19:06:30 +0300

    Добавьте файлы проекта.

---
+++ delegatorApi.Library/SqlDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace delegatorApi.Library
+{
+    internal class SqlDataAccess
+    {
+        public string GetConnectionString(string name) => ConfigurationManager.ConnectionStrings[name].ConnectionString;
+
+        public List<T> LoadData<T, U>(string storedProcedure, U parametrs, string connectionStringName)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString(connectionStringName)))
+            {
+                List<T> rows = cnn.Query<T>(storedProcedure, parametrs, 
+                    commandType: CommandType.StoredProcedure).ToList();
+
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string storedProcedure, T parametrs, string connectionStringName)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString(connectionStringName)))
+            {
+                cnn.Execute(storedProcedure, parametrs,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+    }
+}

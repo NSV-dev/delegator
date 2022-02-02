1f0216a35c511f9cb89a23e024806692851f9fe9
nsv-dev <nsvkart@bk.ru>
2022-02-02 19:35:48 +0300

    Added AppUserData class

---
+++ delegatorApi.Library/DataAccess/AppUserData.cs
@@ -0,0 +1,21 @@
+﻿using delegatorApi.Library.Models;
+using delegatorApi.Library.Models.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegatorApi.Library.DataAccess
+{
+    public class AppUserData
+    {
+        public AppUser GetUserById(string Id)
+        {
+            using (delegatorContext db = new())
+            {
+                return db.AppUsers.First(u => u.Id == Id);
+            }
+        }
+    }
+}
